1593d1cb2ff584bb6badb00989b48c6f7706b3fc
Iuri <iurims@gmail.com>
2014-05-24 19:12:05 -0300

    add chaves testes no modo buy page loja

--- lib/generators/templates/cielo.rb
+++ lib/generators/templates/cielo.rb
@@ -5,11 +5,14 @@ Cielo.setup do |config|
   # config.environment = :test
   
   # Número de afiliação fornecido pela cielo.
-  # O numero padrão é o número usado para testes.
+  # O numero padrão é o número usado para testes. 
+  # Utilize "1001734898" para testes Buy page Cielo e "1006993069" para Buy page loja
   # config.numero_afiliacao = "1001734898"
   
   # Chave de acesso para autenticação.
   # O número padrão é o número usado para os testes.
+  # hash para Buy Page Cielo: "e84827130b9837473681c2787007da5914d6359947015a5cdb2b8843db0fa832"
+  # hash para Buy page Loja: "25fbb99741c739dd84d7b06ec78c9bac718838630f30b112d033ce2e621b34f3"
   # config.chave_acesso = "e84827130b9837473681c2787007da5914d6359947015a5cdb2b8843db0fa832"
   
   # Após o processamento pela cielo, o usuário será redirecionado para uma página.
